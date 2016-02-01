bc64cb349c694f18360ac61b7ee56d8e723feb49
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2016-02-02 09:25:33 +1100

    Initial upload of setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='zonal_stats',
+      version='0.1b',
+      packages=['zonal_stats'],
+      package_dir={'zonal_stats': 'zonal_stats'},
+      scripts=['query/class_db_query.py', 'query/query.py',
+               'workflow/classifier_workflow.py',
+               'workflow/workflow.py',
+               'workflow/combine_stats.py'],
+      author='Josh Sixsmith',
+      license='Apache 2.0')
