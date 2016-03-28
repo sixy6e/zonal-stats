76c03eac4d6e78d3ad7a82c6d31fe589b69f8bb1
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2016-03-29 10:14:51 +1100

    Added the ABS classifier.

--- classifier.py
+++ classifier.py
@@ -5,7 +5,13 @@ import numpy
 from datacube.api.utils import get_dataset_data_with_pq, TCI_COEFFICIENTS
 from datacube.api.utils import calculate_tassel_cap_index, TasselCapIndex
 from datacube.api.utils import calculate_ndvi
-from datacube.api.utils import get_dataset_metadata
+from datacube.api.utils import read_dataset_data, get_dataset_metadata
+from datacube.api.utils import PqaMask, WofsMask
+from datacube.api.utils import get_mask_pqa, get_mask_wofs
+from datacube.api.model import Fc25Bands
+from datacube.api.model import DatasetType
+from eotools.tiling import generate_tiles
+from idl_functions import histogram
 
 
 def classifier(arg25_dataset, pq25_dataset):
@@ -100,5 +106,78 @@ def classifier(arg25_dataset, pq25_dataset):
     # set any nulls
     valid = numpy.isfinite(ndvi)
     classified[~valid] = 0
+
+    return classified
+
+
+def classify_abs(tiles):
+    """
+    Runs the classifier for the ABS FC deciles project.
+    """
+    bands = [Fc25Bands.BARE_SOIL,
+             Fc25Bands.NON_PHOTOSYNTHETIC_VEGETATION,
+             Fc25Bands.PHOTOSYNTHETIC_VEGETATION]
+
+    # PQ bits to mask
+    bits = [PqaMask.PQ_MASK_SATURATION,
+            PqaMask.PQ_MASK_CONTIGUITY,
+            PqaMask.PQ_MASK_CLOUD]
+
+    # array metadata
+    metadata = get_dataset_metadata(tiles[0].datasets[DatasetType.PQ25])
+    cols, rows = metadata.shape
+
+    # allocate classified result
+    classified = {}
+    for band in bands:
+        classified[band] = numpy.zeros((rows, cols), dtype='int8')
+
+    chunks = generate_tiles(cols, rows, cols, 100, False)
+
+    for chunk in chunks:
+        ys, ye = chunk[0]
+        xs, xe = chunk[1]
+        ysize = ye - ys
+        xsize = xe - xs
+        dims = (len(tiles), ysize, xsize)
+
+        # allocate 3D block to read into
+        time_data = {}
+        for band in bands:
+            time_data[band] = numpy.zeros(dims)
+
+        for z, tile in enumerate(tiles):
+            # extract pq
+            mask = get_mask_pqa(tile.datasets[DatasetType.PQ25], bits,
+                                x=xs, y=ys, x_size=xsize, y_size=ysize)
+
+            # check for water
+            if DatasetType.WATER in ds.datasets:
+                mask = get_mask_wofs(tile.datasets[DatasetType.WATER],
+                                     x=xs, y=ys, x_size=xsize, y_size=ysize,
+                                     mask=mask)
+
+            # extract FC data
+            data = read_dataset_data(tiles[0].datasets[DatasetType.FC25],
+                                     bands=bands, x=xs, y=ys,
+                                     x_size=xsize, y_size=ysize)
+
+            # insert and mask
+            for key in data:
+                time_data[key][z] = data[key]
+                time_data[key][z][mask] = numpy.nan
+                xbar = numpy.nanmean(time_data[key], axis=0)
+                h = histogram(xbar, minv=0, maxv=10000, binsize=1000,
+                              locations='loc', omin='omin', omax='omax',
+                              reverse_indices='ri')
+                subs = (classified[key][ys:ye, xs:xe]).ravel()
+                hist = h['histogram']
+                ri = h['ri']
+
+                bins = h['loc'] / 1000
+                for i in bins:
+                    if hist[i] == 0:
+                        continue
+                    subs[ri[ri[i]:ri[i+1]]] = i + 1 # apply in place
 
     return classified
